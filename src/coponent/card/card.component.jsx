9ca4e95d9a78d393dc7df2997a441f06b4d2f7a9
agatatalita <agatamonikaandrzejewska@gmail.com>
2020-04-15 17:37:27 +0200

    fix image loading

--- src/coponent/card/card.component.jsx
+++ src/coponent/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export  const Card = ({monster}) => {
     return (<div className='card-container'>
-        <img alt="monster" src={`https://robohash.org/${monster.id}?set=set1&size=180x180`}/>
+        <img alt="monster" src={`https://robohash.org/${monster.id + 5}?set=set1&size=180x180`}/>
         <h2>{monster.name}</h2>
         <p>{monster.email}</p>
     </div>)
