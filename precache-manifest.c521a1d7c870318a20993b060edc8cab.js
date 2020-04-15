192bf1d67e1a5507d396f349a4af6cda6f17a224
agatatalita <agatamonikaandrzejewska@gmail.com>
2020-04-15 17:27:42 +0200

    Updates

---
+++ precache-manifest.c521a1d7c870318a20993b060edc8cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea15c5132bae4fa3b7455d801f7b539e",
+    "url": "/potwory/index.html"
+  },
+  {
+    "revision": "fd64e850526f7aee7f19",
+    "url": "/potwory/static/css/main.c0246370.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/potwory/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/potwory/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd64e850526f7aee7f19",
+    "url": "/potwory/static/js/main.800a38f2.chunk.js"
+  },
+  {
+    "revision": "fb137d2977456f676620",
+    "url": "/potwory/static/js/runtime-main.25e34f85.js"
+  }
+]);
